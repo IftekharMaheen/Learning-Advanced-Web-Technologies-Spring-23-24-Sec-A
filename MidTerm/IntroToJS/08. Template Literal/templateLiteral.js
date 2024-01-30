82a7ecfcbb1268a2a0d09e25ca624aa7a6aa1c8c
Iftekhar Uddin Mullick <infomullick2000@gmail.com>
2024-01-30 22:12:05 +0600

    Complete tasks of Labtask1 - Template Literal

---
+++ MidTerm/IntroToJS/08. Template Literal/templateLiteral.js
@@ -0,0 +1,15 @@
+const name = "Alice";
+const greeting = `Hello, ${name}!`;
+console.log(greeting);
+
+// Multiline string
+const multiline = `this is
+a multiline
+string.`;
+console.log(multiline);
+
+// String Interpolation
+const a = 5;
+const b = 10;
+const sum = `The sum of ${a} and ${b} is ${a + b}.`;
+console.log(sum);
