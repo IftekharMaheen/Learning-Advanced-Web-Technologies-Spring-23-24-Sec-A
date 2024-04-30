25940afb9c3b09e7a202c4006a0317c02f025772
IftekharUddinMullick <infomullick2000@gmail.com>
2024-04-30 23:42:11 +0600

    Header and Footer added

---
+++ FinalTerm-FullStackTask-ProductEcom/frontend/src/components/Navbar/NavBar.tsx
@@ -0,0 +1,26 @@
+import Link from 'next/link';
+import Logo from '../../../public/images/logo.jpg';
+import Image from 'next/image';
+
+const NavBar = () => 
+    {
+        return (
+            <div className="nav">
+            
+            <div className="nav-logo">
+                <Link href='/'><Image src={Logo} alt="logo" width={50} height={50} className='logo' /> </Link>
+            </div>
+           
+           <div className="nav-linker">
+            <ul>
+                <li className="nav-links"><Link href='./register'>Register</Link> </li>
+                <li className="nav-links"><Link href='./login'>Login</Link></li>
+            </ul>
+            </div>
+            
+
+            </div>
+        )
+    }
+
+    export default NavBar;
